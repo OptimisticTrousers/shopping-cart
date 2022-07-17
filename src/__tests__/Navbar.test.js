91b927f891f99c2a5f28b885f939545f27eac01b
OptimisticTrousers <tony.isern@yahoo.com>
2022-07-17 10:38:05 -0400

    Restructuring file structure

--- src/__tests__/Navbar.test.js
+++ src/__tests__/Navbar.test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {render, screen} from '@testing-library/react'
 import '@testing-library/jest-dom'
 import userEvent from '@testing-library/user-event'
-import Navbar from '../Navbar'
+import Navbar from '../components/Navbar'
 
 it("renders navbar", () => {
 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './assets/global.css'
+import './global.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Shop from './pages/Shop';
+import Shop from './components/Shop';
+import Navbar from './components/Navbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/shop" element={<Shop />} />
+      <Route path="shop" element={<Shop />} />
     </Routes>
     </BrowserRouter>
   </React.StrictMode>
