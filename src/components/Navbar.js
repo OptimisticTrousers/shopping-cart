4e4c52ef033d5d51d8d041eb332b4a3456ff3db5
OptimisticTrousers <tony.isern@yahoo.com>
2022-07-17 14:50:44 -0400

    Adding svgs to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,12 @@
+import shoppingCart from '../assets/shopping_cart.svg'
+import logo from '../assets/optimistictrousers.jpg'
+
 export default function Navbar() {
     return (
         <nav>            
             <div className="links">
-                
+                <img src={logo} alt="smiling pair of pants"/>
+                <img src={shoppingCart} alt="shopping cart"/>
             </div>
         </nav>
     )
