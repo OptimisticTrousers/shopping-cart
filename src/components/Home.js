215c6b781708325b80a240db20462bffc32997b0
OptimisticTrousers <tony.isern@yahoo.com>
2022-07-17 15:20:03 -0400

    Adding links to navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,11 @@
+import Hero from "./Hero"
+import About from "./About"
+
 export default function Home() {
     return (
         <div className="home">
-            
-            
+            <Hero /> 
+            <About />
         </div>
     )
 }
