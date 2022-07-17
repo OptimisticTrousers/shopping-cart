9003a9f3225579bb2df4ad1916ca9196c40608a0
OptimisticTrousers <tony.isern@yahoo.com>
2022-07-17 09:19:03 -0400

    Using react router

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './assets/global.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Shop from './pages/Shop';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/shop" element={<Shop />} />
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
