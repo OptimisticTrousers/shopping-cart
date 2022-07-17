5ae64ac02c30d98b20c0f920412a14fe3e8983b9
OptimisticTrousers <tony.isern@yahoo.com>
2022-07-17 11:36:47 -0400

    Making an API request to the Fake Store API

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,9 +1,20 @@
+import React, {useEffect} from "react"
+import ProductCard from "./ProductCard"
+
 export default function Shop() {
+
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products')
+            .then(res => res.json())
+            .then(json => console.log(json))
+    }, [])
+
     return (
         <main>
             <h1>Get Clothing!</h1>
             <p>Times are tough. Liven up with our top-notch clothing!</p>
             <div className="products">
+                <ProductCard />
 
             </div>
         </main>
