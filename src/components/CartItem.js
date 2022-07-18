6e84207f5850838783661efe958996c8a082022d
OptimisticTrousers <tony.isern@yahoo.com>
2022-07-18 17:55:56 -0400

    Making checkout responsive

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -2,11 +2,11 @@ import trash from '../assets/trash.svg'
 
 export default function CartItem({image, title, price, quantity}) {
     return (
-        <div className="card-item-container">
+        <div className="cart-item-container">
             <div className="item-image">
                 <img src={image} alt={title}/>
             </div>
-            <div className="card-item-details">
+            <div className="cart-item-details">
                 <div className="item-detail">
                     <h2 className='item-title'>Title</h2>
                     <p className='item-price'>Price</p>
