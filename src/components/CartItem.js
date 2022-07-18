c0ed7ada2f53dce71a397667021bff42611c4ee4
OptimisticTrousers <tony.isern@yahoo.com>
2022-07-18 16:43:00 -0400

    Adding a trash.svg

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,3 +1,5 @@
+import trash from '../assets/trash.svg'
+
 export default function CartItem({image, title, price, quantity}) {
     return (
         <div className="card-item-container">
@@ -15,7 +17,10 @@ export default function CartItem({image, title, price, quantity}) {
                         <p className="quantity-display">0</p>
                         <button type="button">-</button>
                     </div>
-                    <p>Delete</p>
+                    <div className="delete">
+                        <img src={trash} alt="trash can"/>
+                        <span>Delete</span>
+                    </div>
                 </div>
             </div>
         </div>
