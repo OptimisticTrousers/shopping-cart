072362b2c60fb52f1011745eab6d5d46eb6837a0
OptimisticTrousers <tony.isern@yahoo.com>
2022-07-18 14:54:40 -0400

    Rendering individual pages for each product dynamically

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,7 +1,27 @@
-export default function ProductDetail(){
+import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
+
+export default function ProductDetail(props){
+
+    const params = useParams()
+
+    const [details, setDetails] = useState({})
+
+    useEffect(() => {
+        fetch(`https://fakestoreapi.com/products/${params.id}`)
+            .then(res => res.json())
+            .then(data => setDetails(data))
+    })
+
+    console.log(details)
+
     return (
         <div>
-            <h1>Stuff</h1>
+            <h1>{details.title}</h1>
+            <p>{details.price}</p>
+            <p>{details.category}</p>
+            <p>{details.description}</p>
+            <img src={details.image} />
         </div>
     )
 }
