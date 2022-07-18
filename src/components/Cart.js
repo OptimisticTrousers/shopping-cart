58c5821ae3e0978983507cb7a21d3098327a3cbd
OptimisticTrousers <tony.isern@yahoo.com>
2022-07-18 17:15:37 -0400

    Adding costs at the bottom of checkout

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -12,8 +12,23 @@ export default function Cart() {
                 <CartItem image={logo}/>
                 <CartItem image={logo}/>
                 <CartItem image={logo}/>
+                <div className="cost">
+                    <p className="subtotal">Subtotal</p>
+                    <p className="amount">$400.99</p>
+                </div>
+                <div className="cost">
+                    <p className="delivery">Delivery</p>
+                    <p className="amount">$0.00</p>
+                </div>
+                <div className="cost">
+                    <p className="tax">Tax</p>
+                    <p className="amount">+ $14.00</p>
+                </div>
+                <div className="cost">
+                    <p className="total">Total</p>
+                    <p className="amount">$78.13</p>
+                </div>
             </div>
-            <p>SUBTOTAL</p>
             <div className="buttons">
                 <button type="button">Check Out</button>
                 <button type="button">Back To Products</button>
