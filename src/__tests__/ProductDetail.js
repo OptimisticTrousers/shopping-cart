b8949a1c18c165d2282368846183064e2587a75a
OptimisticTrousers <tony.isern@yahoo.com>
2022-07-20 07:53:11 -0400

    Testing ProductDetail

---
+++ src/__tests__/ProductDetail.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {render, screen} from '@testing-library/react'
+import '@testing-library/jest-dom'
+import userEvent from '@testing-library/user-event'
+import Shop from '../components/Shop'
+import ProductDetail from '../components/ProductDetail'
+import {CartQuantityProvider}from '../hooks/QualityContext'
+
+it('the user incrementing the amount of items in the shopping cart', async () => {
+
+    const user = userEvent.setup()
+
+    render(<CartQuantityProvider><ProductDetail/></CartQuantityProvider>)
+
+    await user.click(screen.getByRole('button', {name: /Add to Cart/i}))
+
+    const counter = screen.getByRole('span', {name: "quantity"})
+
+    expect(counter).toEqual(1)
+})
