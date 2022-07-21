5895c09c5388386f6769ed6283505aa71b179f60
OptimisticTrousers <tony.isern@yahoo.com>
2022-07-21 16:52:26 -0400

    Cleaning up ProductDetail test
    Adding more stuff

---
+++ src/__tests__/Navbar.test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {render, screen} from '@testing-library/react'
+import '@testing-library/jest-dom'
+import userEvent from '@testing-library/user-event'
+import Shop from '../components/Shop';
+
+describe("Navbar", () => {
+    it("rendering product count correctly near shopping cart", () => {
+        render()
+    })
+})
--- src/__tests__/ProductDetail.test.js
+++ src/__tests__/ProductDetail.test.js
@@ -1,11 +1,10 @@
-import React, { useContext, useState } from 'react'
 import {render, screen} from '@testing-library/react'
 import '@testing-library/jest-dom'
 import userEvent from '@testing-library/user-event'
 import Shop from '../components/Shop'
 import ProductDetail from '../components/ProductDetail'
 import * as Store from '../context/Store'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
 import Navbar from '../components/Navbar'
 
 describe("ProductDetail", () => {
@@ -16,23 +15,9 @@ describe("ProductDetail", () => {
             useParams: () => ({
                 id: 1
             }),
-            useRouteMatch: () => ({url: 'shop/1'})
         }))
     })
     it("expecting to call the 'Add to Cart' function in the cart context", async () => {
-
-        const DummyContext = React.createContext()
-
-        const DummyCartProvider = jest.fn(({children}) => {
-
-            const [dummyCart, setDummyCart] = useState([])
-
-            return (
-                <DummyContext.Provider value={dummyCart}>
-                    {children}
-                </DummyContext.Provider>
-            )
-        })
 
         const mockAddToCart = jest.fn()
         jest.spyOn(Store, 'useAddToCart').mockReturnValue(mockAddToCart)
@@ -61,26 +46,6 @@ describe("ProductDetail", () => {
 
         expect(mockAddToCart).toHaveBeenCalledTimes(7)
     })
-    it.skip('the user incrementing the amount of items in the shopping cart', async () => {
-
-
-        const user = userEvent.setup()
-
-        render(
-            <BrowserRouter>
-                <Store.CartProvider>
-                    <Navbar />
-                    <ProductDetail/>
-                </Store.CartProvider>
-            </BrowserRouter>
-        )
-
-        await user.click(screen.getByRole('button', {name: /Add to Cart/i}))
-        await user.click(screen.getByRole('button', {name: /Add to Cart/i}))
-
-        const counter = screen.queryByTestId('quantity')
-
-        expect(counter.textContent).toEqual("2")
-    })
+    
 
 })
