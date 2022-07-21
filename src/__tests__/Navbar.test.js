ff12869f190523280315bc45fb7c01350c058b18
OptimisticTrousers <tony.isern@yahoo.com>
2022-07-21 16:56:05 -0400

    Adding a test to Navbar

--- src/__tests__/Navbar.test.js
+++ src/__tests__/Navbar.test.js
@@ -2,10 +2,30 @@ import React from 'react'
 import {render, screen} from '@testing-library/react'
 import '@testing-library/jest-dom'
 import userEvent from '@testing-library/user-event'
-import Shop from '../components/Shop';
+import BrowserRouter from 'react-router-dom'
+import { CartProvider } from '../context/Store'
+import Navbar from '../components/Navbar'
+import ProductDetail from '../components/ProductDetail'
 
 describe("Navbar", () => {
-    it("rendering product count correctly near shopping cart", () => {
-        render()
+    it('the user incrementing the amount of items in the shopping cart', async () => {
+
+        render(
+            <BrowserRouter>
+                <CartProvider>
+                    <Navbar />
+                    <ProductDetail/>
+                </CartProvider>
+            </BrowserRouter>
+        )
+
+        const user = userEvent.setup()
+
+        await user.click(screen.getByRole('button', {name: /Add to Cart/i}))
+        await user.click(screen.getByRole('button', {name: /Add to Cart/i}))
+
+        const counter = screen.queryByTestId('quantity')
+
+        expect(counter.textContent).toEqual("2")
     })
 })
