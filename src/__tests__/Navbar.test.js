3dfaf51f95c4397fb84959fc5dac9797cf4b331c
OptimisticTrousers <tony.isern@yahoo.com>
2022-07-21 18:39:04 -0400

    Using fetch each and mocking fetch API

--- src/__tests__/Navbar.test.js
+++ src/__tests__/Navbar.test.js
@@ -6,6 +6,7 @@ import {BrowserRouter} from 'react-router-dom'
 import { CartProvider } from '../context/Store'
 import Navbar from '../components/Navbar'
 import ProductDetail from '../components/ProductDetail'
+import each from 'jest-each'
 
 describe("Navbar", () => {
     beforeAll(() => {
@@ -15,7 +16,27 @@ describe("Navbar", () => {
                 id: 1
             }),
         }))
+
     })
+
+    beforeEach(() => {
+
+        fetch.mockClear()
+
+        fetch.mockResponseOnce(JSON.stringify({
+                    id: 1,
+                    title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+                    price: 109.95,
+                    description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+                    category: "men's clothing",
+                    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+                    rating: {
+                    rate: 3.9,
+                    count: 120
+                    }
+                }))
+    })
+
     it("snapshot", () => {
         const {asFragment} = render(
             <BrowserRouter>
@@ -27,7 +48,14 @@ describe("Navbar", () => {
 
         expect(asFragment()).toMatchSnapshot()
     })
-    it('the user incrementing the amount of items in the shopping cart', async () => {
+    each([
+        7,
+        1,
+        2,
+        5,
+        10,
+        3
+    ]).it('the user incrementing the amount of items in the shopping cart', async (userClicks) => {
 
         render(
             <BrowserRouter>
@@ -40,12 +68,14 @@ describe("Navbar", () => {
 
         const user = userEvent.setup()
 
-        await user.click(screen.getByRole('button', {name: /Add to Cart/i}))
-        await user.click(screen.getByRole('button', {name: /Add to Cart/i}))
-        await user.click(screen.getByRole('button', {name: /Add to Cart/i}))
+        for(let i = 0; i < userClicks; i++){
+
+            await user.click(screen.getByRole('button', {name: /Add to Cart/i}))
+        }
 
         const counter = screen.queryByTestId('quantity')
 
-        expect(counter.textContent).toEqual("3")
+        expect(counter.textContent).toEqual(userClicks.toString())
+        expect(fetch).toHaveBeenCalledTimes(1)
     })
 })
