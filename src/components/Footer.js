5b7f72e914a997fb792ebf85cfb3168766b7dbc7
OptimisticTrousers <tony.isern@yahoo.com>
2022-07-22 16:57:12 -0400

    Making footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,26 +5,28 @@ export default function Footer() {
         <footer>
             <div className="footer-content">
                 <div className="newsletter-input">
-                    <h2>Newsletter</h2>
+                    <p className='newsletter-description'>Subscribe to our newsletter!</p>
+                    <h2 className='newsletter-title'>NEWSLETTER</h2>
                     <input placeholder="Email"></input>
+                    <button className="newsletter-button" type="submit">Subscribe</button>
                 </div>
-                <button className="newsletter-button" type="submit">Subscribe</button>
                 <div className="contacts">
                     <h2 className="contacts-title">Contacts</h2>
                     <p className="phone-text">Phone: <span>555-999-1111</span></p>
-                    <p className="email-text">Email: <span>Bob Jones</span></p>
+                    <p className="email-text">Email: <span>bobjones@gmail.com</span></p>
                     <p className="address-text">Address: <span>Planet Earth, Milky Way</span></p>
                 </div>
                 <div className="social">
                     <h2 className="contacts-title">Social</h2>
-                    <a href="https://github.com/OptimisticTrousers">
-                        <img className="logo" src={githubLogo} alt="github-logo" />
-                    </a>
-                    <br />
-                    <a href="https://www.linkedin.com/in/tony-isern-9717a0189">
-                        <img className="logo" src={linkedInLogo} alt="linkedin-logo" />
-                    </a>
-                    <p>Check out our social media!</p>
+                    <div className='logos'>
+                        <a href="https://github.com/OptimisticTrousers">
+                            <img className="github-logo" src={githubLogo} alt="github-logo" />
+                        </a>
+                        <br />
+                        <a href="https://www.linkedin.com/in/tony-isern-9717a0189">
+                            <img className="linkedin-logo" src={linkedInLogo} alt="linkedin-logo" />
+                        </a>
+                    </div>
                 </div>
             </div>
         </footer>
