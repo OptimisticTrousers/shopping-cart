a1e6fd03ffd18bab6c002ec07cea56951b4ecbc6
OptimisticTrousers <tony.isern@yahoo.com>
2022-07-22 17:04:42 -0400

    Fixing imports in Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,9 @@
 import Hero from "./Hero"
-import About from "./About"
 
 export default function Home() {
     return (
         <div className="home">
             <Hero /> 
-            <About />
         </div>
     )
 }
