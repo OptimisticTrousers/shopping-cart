bb4295120448c96479d047c0717d6ec107ac3f71
OptimisticTrousers <tony.isern@yahoo.com>
2022-07-21 20:37:01 -0400

    Writing tests for CartItem

--- src/__tests__/CartItem.test.js
+++ src/__tests__/CartItem.test.js
@@ -9,6 +9,9 @@ import Home from '../components/Home'
 import Shop from '../components/Shop'
 import Cart from '../components/Cart'
 import ProductDetail from '../components/ProductDetail'
+import Navbar from '../components/Navbar'
+import each from 'jest-each'
+import { CartProvider } from '../context/Store';
 
 describe('CartItem', () => {
 
@@ -43,6 +46,43 @@ describe('CartItem', () => {
         expect(productQuantity.textContent).toBe(quantity.toString())
         expect(productPrice.textContent).toBe(`$${price}`)
 
+    })
+
+    each([
+        2, 
+        3,
+        1,
+        5,
+        10,
+        7
+    ]).it('correctly increments total cart quantity', async (userClicks) => {
+
+        const {title, price, image, rating, quantity} = product
+
+        render(
+            <BrowserRouter>
+                <CartProvider>
+                    <Navbar /> 
+                    <CartItem title={title} price={price} image={image} rating={rating} quantity={quantity}/>
+                </CartProvider>
+            </BrowserRouter>
+        )
+
+        const user = userEvent.setup()
+
+        const incrementProductQuantityButton = screen.getByRole('button', {name: "+"})
+
+        for(let i = 0; i < userClicks; i++){
+
+            await user.click(incrementProductQuantityButton)
+        }
+
+        const productQuantity = screen.queryByTestId('product-quantity')
+
+        const navQuantity = screen.queryByTestId('quantity')
+
+        expect(navQuantity.textContent).toBe(userClicks.toString())
+        expect(navQuantity.textContent).toBe(productQuantity.textContent)
     })
 
 })
