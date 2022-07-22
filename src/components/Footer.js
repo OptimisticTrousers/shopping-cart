afbbd017afb0d56df5fa30e1cb8a9e41af0d4227
OptimisticTrousers <tony.isern@yahoo.com>
2022-07-22 16:43:47 -0400

    Adding logos

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,5 @@
+import githubLogo from '../assets/github-logo.svg'
+import linkedInLogo from '../assets/linkedin-logo.svg'
 export default function Footer() {
     return (
         <footer>
@@ -8,20 +10,23 @@ export default function Footer() {
                 </div>
                 <button className="newsletter-button" type="submit">Subscribe</button>
                 <div className="contacts">
+                    <h2 className="contacts-title">Contacts</h2>
                     <p className="phone-text">Phone: <span>555-999-1111</span></p>
                     <p className="email-text">Email: <span>Bob Jones</span></p>
                     <p className="address-text">Address: <span>Planet Earth, Milky Way</span></p>
                 </div>
                 <div className="social">
-                    <h2>Icon 1</h2>
-                    <h2>Icon 1</h2>
-                    <h2>Icon 1</h2>
+                    <h2 className="contacts-title">Social</h2>
+                    <a href="https://github.com/OptimisticTrousers">
+                        <img className="logo" src={githubLogo} alt="github-logo" />
+                    </a>
+                    <br />
+                    <a href="https://www.linkedin.com/in/tony-isern-9717a0189">
+                        <img className="logo" src={linkedInLogo} alt="linkedin-logo" />
+                    </a>
                     <p>Check out our social media!</p>
                 </div>
             </div>
-            <a href="https://github.com/OptimisticTrousers">
-                Created by Tony Isern 
-            </a>
         </footer>
     )
 }
