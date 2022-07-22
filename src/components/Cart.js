9e89d52263051a841e60ca2477fbbe505b6134c5
OptimisticTrousers <tony.isern@yahoo.com>
2022-07-22 14:36:33 -0400

    Fixing router

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -21,11 +21,21 @@ export default function Cart() {
         return <CartItem key={id} id={id} image={image} price={price} title={title} quantity={quantity} description={description} rating={rating} category={category} />
     })
 
+    const emptyCart = (
+        <div className="empty-cart">
+            <div className="empty-cart-image">
+                <svg data-testid="cart-svg" width="20" height="22" viewBox="0 0 20 22" fill="none" stroke="currentColor"><path d="M4 1L1 5V19C1 19.5304 1.21071 20.0391 1.58579 20.4142C1.96086 20.7893 2.46957 21 3 21H17C17.5304 21 18.0391 20.7893 18.4142 20.4142C18.7893 20.0391 19 19.5304 19 19V5L16 1H4Z" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path><path d="M1 5H19" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path><path d="M14 9C14 10.0609 13.5786 11.0783 12.8284 11.8284C12.0783 12.5786 11.0609 13 10 13C8.93913 13 7.92172 12.5786 7.17157 11.8284C6.42143 11.0783 6 10.0609 6 9" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path></svg>
+            </div>
+            <h1 className="empty-cart-title">Your cart is empty</h1>
+            <p className="empty-cart-description">Biscuit oat cake wafer icing ice cream tiramisu pudding cupcake.</p>
+        </div>
+    )
+
     return (
         <div className="cart-container">
             <div className="cart">
                 <h2 className="cart-title">Your Cart</h2>
-                {cart.length ? renderedCart : <p>Stuff</p>}
+                {cart.length ? renderedCart : emptyCart}
                 <div className="cost">
                     <p className="subtotal">Subtotal</p>
                     <p className="amount" data-testid="subtotal">${subTotal}</p>
