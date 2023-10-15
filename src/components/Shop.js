d28d4c1916673f3aa1ae0708310860352752de50
OptimisticTrousers <tony.isern1@gmail.com>
2023-10-15 02:47:44 -0400

    Making design changes

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,24 +1,35 @@
-import React from "react"
-import ProductCard from "./ProductCard"
-import { Link, useOutletContext } from "react-router-dom"
+import React from "react";
+import ProductCard from "./ProductCard";
+import { Link, useOutletContext } from "react-router-dom";
 
 export default function Shop() {
+  const products = useOutletContext();
 
-    const products = useOutletContext()
-
-    const renderedProducts = products.map((product) => {
-        return <Link key={product.id} to={`${product.id}`} state={{product}}>
-            <ProductCard  image={product.image} rating={product.rating} title={product.title} price={product.price}/>
-        </Link>
-    })
-
+  const renderedProducts = products.map((product, index) => {
     return (
-        <div className="shop">
-            <h2 className="shop-title">Get Clothing!</h2>
-            <p className="shop-description">Times are tough. Liven up with our top-notch clothing!</p>
-            <div className="products">
-                {renderedProducts}
-            </div>
-        </div>
-    )
+      <Link
+        key={product.id}
+        to={`${product.id}`}
+        state={{ product }}
+        style={{ "--item-number": index }}
+      >
+        <ProductCard
+          image={product.image}
+          rating={product.rating}
+          title={product.title}
+          price={product.price}
+        />
+      </Link>
+    );
+  });
+
+  return (
+    <div className="shop">
+      <h2 className="shop-title">Get Clothing!</h2>
+      <p className="shop-description">
+        Times are tough. Liven up with our top-notch clothing!
+      </p>
+      <div className="products">{renderedProducts}</div>
+    </div>
+  );
 }
